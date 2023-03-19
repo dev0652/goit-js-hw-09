22f3d2f0eac1dcfe541e96ae9773208ed74e804a
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-03-19 21:39:49 +0000

    task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -10,27 +10,20 @@ refs.stopBtn.addEventListener('click', stopColorChange);
 let intervalId = null;
 
 function startColorChange() {
-  console.log('start clicked');
-
   if (!intervalId) {
-    // intervalId = setInterval(changeBgColor, 1000, getRandomHexColor());
     intervalId = setInterval(changeBgColor, 1000);
-
     refs.startBtn.disabled = true;
   }
 }
 function stopColorChange() {
-  console.log('stop clicked');
-
   clearInterval(intervalId);
   intervalId = null;
-  // changeBgColor('white');
+  refs.body.removeAttribute('style');
   refs.startBtn.disabled = false;
 }
 
-function changeBgColor() {
-  refs.body.style.backgroundColor = getRandomHexColor();
-  console.log('fired');
+function changeBgColor(color = getRandomHexColor()) {
+  refs.body.style.backgroundColor = color;
 }
 
 function getRandomHexColor() {
@@ -38,23 +31,3 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-// const colorChanger = {
-//   intervalId: setInterval(() => {
-//     refs.body.style.backgroundColor = this.getRandomHexColor();
-//   }, 1000),
-
-//   start() {
-//     this.intervalId();
-//   },
-
-//   stop() {
-//     clearInterval(this.intervalId);
-//   },
-
-//   getRandomHexColor() {
-//     return `#${Math.floor(Math.random() * 16777215)
-//       .toString(16)
-//       .padStart(6, 0)}`;
-//   },
-// };
